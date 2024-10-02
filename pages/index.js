1cbd391097c80006eb07f94e2384584df6dab26b
andrecj2002 <joaoajorge@ua.ptm>
2024-10-02 10:19:00 +0100

    Added prettier

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,14 @@ export default function Home() {
               Get started by editing <code>pages/index.js</code>.
             </li>
             <li>Save and see your changes instantly.</li>
+            <li> Teste de prettier</li>
+            <li>mais um teste </li>
+            <li> Teste de prettier</li>
+            <li>maais um teste </li>
+            <li> Teste de prettier</li>
+            <li>maais um teste </li>
+            <li> Teste de prettier</li>
+            <li>maais um teste </li>
           </ol>
 
           <div className={styles.ctas}>
